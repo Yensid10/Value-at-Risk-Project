705d8d32e5e280ceadb23dbad911209a84a5a181
Benjamin Shearlock <ben.shearlock@gmail.com>
2023-11-13 15:17:48 +0000

    Changed Historical to use Percentile function
    Realised that the most accurate way to calculate the historical simulation VaR would be to use the percentile function, to find the exact percentile needed, so I implemented it.

--- Historical.py
+++ Historical.py
@@ -12,20 +12,18 @@ with open('NKE.csv', 'r') as file:
             continue
         closes = np.append(closes, float(row[5]))
         
-# print(closes)
 diffs = np.array([])
 for i in range(1, len(closes)): 
     diffs = np.append(diffs, (closes[i]/closes[i-1] - 1))
-# print(round(np.partition(diffs, 24)[24], 3))
-print("500 Day stock history, for 1 day time horizon, 95% certainty, Portfolio of 100,000,000, VaR for the 23/10/2023: £" + str(round(np.partition(diffs, 24)[24]*100000000*-1, 3)))
+    
+print("500 Day stock history, for 1 day time horizon, 95% certainty, Portfolio of 100,000,000, VaR for the 23/10/2023: £" + str(round(np.percentile(diffs, 5)*100000000*-1, 3)))
 
 count = 0
 for i in range(1, len(closes) - 100):
-    backtest = np.array([])
+    backTest = np.array([])
     for x in range(i, i + 100):
-        backtest = np.append(backtest, (closes[x]/closes[x-1] - 1))
-    VaR = np.partition(backtest, 4)[4]
+        backTest = np.append(backTest, (closes[x]/closes[x-1] - 1))
+    VaR = np.percentile(backTest, 5)
     if VaR > ((closes[x+1]/closes[x] - 1)):
-        # print(str(VaR) + " " + str((closes[x+1]/closes[x] - 1)*100000000))
         count += 1
-print("Backtest for 100-day rolling window of 500 Day stock history, at 95% certainty: " + str(round(count/(len(closes)-100)*100, 1)) + "%")
+print("Back Test for 100-day rolling window of 500 Day stock history, at 95% certainty: " + str(round(count/(len(closes)-100)*100, 1)) + "%")
--- Model.py
+++ Model.py
@@ -3,9 +3,6 @@ import numpy as np
 from scipy.stats import norm
 import yfinance as yf
 
-# print(100000000 - 100000000*(norm.ppf(0.05, 0.00036, 0.0126) + 1))
-#This appears to work for variance - covariance VAR
-
 print("Model Building VaR\n")
 def dailyVar(P, rl, mean, sD):
     return P - P*(norm.ppf(rl, mean, sD) + 1)
@@ -13,11 +10,9 @@ def dailyVar(P, rl, mean, sD):
 if __name__ == "__main__":
     startDate = dt.datetime(2021, 10, 26)
     endDate = dt.datetime(2023, 10, 24)
-
     #format              year, month, day
     
     stock = yf.download('NKE', startDate, endDate)
-    # print(stock)
     closeDiffs = stock['Adj Close'].pct_change()
     
     portfolio = 100000000
@@ -30,11 +25,11 @@ if __name__ == "__main__":
     
     count = 0
     for i in range(1, len(stock) - 100):
-        backtest = np.array([])
-        backtest = stock['Adj Close'].pct_change()[i:i+100]
-        VaR = dailyVar(portfolio, rlPercent, np.mean(backtest), np.std(backtest))
-        day = stock['Adj Close'].pct_change()[i+100:i+101].values[0]
-        if VaR*-1 > (day*portfolio): #Needs to be multiplied by -1 as VaR is a predicted loss
+        backTest = np.array([])
+        backTest = stock['Adj Close'].pct_change()[i:i+100]
+        VaR = dailyVar(portfolio, rlPercent, np.mean(backTest), np.std(backTest))
+        nextDay = stock['Adj Close'].pct_change()[i+100:i+101].values[0]
+        if VaR*-1 > (nextDay*portfolio): #Needs to be multiplied by -1 as VaR is a predicted loss
             count += 1
-    print("Backtest for 100-day rolling window of 500 Day stock history, at 95% certainty: " + str(round(count/(len(stock)-100)*100, 1)) + "%")
+    print("Back Test for 100-day rolling window of 500 Day stock history, at 95% certainty: " + str(round(count/(len(stock)-100)*100, 1)) + "%")
     
