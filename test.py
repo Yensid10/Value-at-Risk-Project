6800390fe34a70ac2fba214f65d2c8f9cdb013a5
Benjamin Shearlock <ben.shearlock@gmail.com>
2023-11-13 03:10:12 +0000

    Implemented Back-Testing
    After reading up on back testing, I decided to attempt to implement it for historical simulation (test1), it performs a rolling window of 100 days and checks the VaR at the end of each 100 days, if it exceeds the VaR loss then its added to a count, then the count is divided into a percentage based on the amount of windows used, seeing how accurate the VaR seems to be. At the 5% level, it appears my VaR is. I also added to Diary.

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 
+print("Historical Simulation VaR\n")
 closes = np.array([])
 with open('NKE.csv', 'r') as file:
     reader = csv.reader(file)
@@ -16,5 +17,15 @@ diffs = np.array([])
 for i in range(2, len(closes)): 
     diffs = np.append(diffs, (closes[i]/closes[i-1] - 1))
 # print(round(np.partition(diffs, 24)[24], 3))
-print(round(np.partition(diffs, 24)[24]*100000000, 3))
+print("500 Day stock history, for 1 day time horizon, 95% certainty, Portfolio of 100,000,000, VaR for the 23/10/2023: £" + str(round(np.partition(diffs, 24)[24]*100000000*-1, 3)))
 
+count = 0
+for i in range(2, len(closes) - 100):
+    backtest = np.array([])
+    for x in range(i, i + 100):
+        backtest = np.append(backtest, (closes[x]/closes[x-1] - 1))
+    VaR = np.partition(backtest, 4)[4]
+    if VaR > ((closes[x+1]/closes[x] - 1)):
+        # print(str(VaR) + " " + str((closes[x+1]/closes[x] - 1)*100000000))
+        count += 1
+print("Backtest for 100-day rolling window of 500 Day stock history, at 95% certainty: " + str(round(count/(len(closes)-100)*100, 1)) + "%")
